92665c6a143db37b1448ff55490b9b3420b70937
Emile Trotignon <emile.trotignon@laposte.net>
2018-12-01 17:55:39 +0100

    prints black and white mandelbrot set

--- main.ml
+++ main.ml
@@ -1,3 +1,7 @@
+#load "graphics.cma";;
+open Graphics
+open Printf
+
 type complex = float * float
 
 let (+%) (xr, xi) (yr, yi) =
@@ -18,4 +22,41 @@ let is_bounded c it =
     n := !n + 1;
   done;
   if !n = it then None
-  else Some it
+  else Some !n
+
+let ft_height x_def y_def width =
+  width *. (float_of_int y_def) /. (float_of_int x_def)
+
+let pixel_to_complex x y x_def y_def (x_center, y_center) width height =
+  let x_corner = x_center -. (width /. 2.) in
+  let y_corner = y_center +. (height /. 2.) in
+  (x_corner, y_corner)
+               
+let draw_mandel_brot_set x_def y_def (x_center, y_center) width it =
+  let height = ft_height x_def y_def width in
+  let x_corner = x_center -. (width /. 2.) in
+  let y_corner = y_center +. (height /. 2.) in
+  let x_pas = width /. (float_of_int x_def) in
+  let y_pas = height /. (float_of_int y_def) in
+  (*printf "%f, %f, %f, %f\n" x_corner y_corner x_pas y_pas;*)
+  open_graph "";
+  let x_complex = ref x_corner in
+  let y_complex = ref y_corner in
+  let bounded = ref (is_bounded (!x_complex, !y_complex) it) in
+  for y = 0 to y_def do
+    for x = 0 to x_def do
+      bounded := is_bounded (!x_complex, !y_complex) it;
+      (*printf "%d %d %f %f" x y !x_complex !y_complex;*)
+      set_color (match !bounded with
+                 | None -> (*printf " None\n";*) black
+                 | Some x -> (*printf " %d\n" x;*) white);
+      plot x y;
+      x_complex := !x_complex +. x_pas;
+    done;
+    y_complex := !y_complex -. y_pas;
+    x_complex := x_corner
+  done
+        
+                    
+                      
+      
